3f77d88bac7106dd13274d9759c2184103cc0548
Branco95 <pv22134@alunos.estgv.ipv.pt>
2023-10-24 11:25:27 +0100

    Added API

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,5 +1,30 @@
-const TodoList = () => {
-  return <div>TodoList</div>;
+import { ITask } from "@/types/tasks";
+
+interface TodoListProps {
+  tasks: ITask[];
+}
+
+const TodoList: React.FC<TodoListProps> = ({ tasks }) => {
+  return (
+    <div className="overflow-x-auto rounded-lg">
+      <table className="table w-full ">
+        <thead className="">
+          <tr className="bg-slate-100">
+            <th>Tasks</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task) => (
+            <tr key={task.id}>
+              <td>{task.text}</td>
+              <td>Blue</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default TodoList;
